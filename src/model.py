0ffc9484178d8c78c4ad017d05e6947fd16fd9c2
Sreekanth Tangirala <sreddy@4basecare.com>
2024-11-22 23:26:01 +0530

    first commit

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super(MNISTModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(8 * 7 * 7, 32)
+        self.fc2 = nn.Linear(32, 10)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 8 * 7 * 7)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+# Add the parent directory to Python path so we can import modules from there
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
